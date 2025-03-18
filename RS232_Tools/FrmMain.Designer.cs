82df163032cbb948ddf5ed8ad78d9b3318c994fd
Shengbi <39143915+Countonme@users.noreply.github.com>
2025-03-18 20:46:14 +0800

    Add project files.

---
+++ RS232_Tools/FrmMain.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace RS232_Tools
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(749, 506);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "RS232 Testing Tools";
+            this.ZoomScaleRect = new System.Drawing.Rectangle(15, 15, 565, 450);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
